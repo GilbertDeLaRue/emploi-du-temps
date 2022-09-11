0daecdc013f74a96226bca4dd666ce1899a44414
Nawfel <56658371+GilbertDeLaRue@users.noreply.github.com>
2022-09-11 20:24:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var cron = require('node-cron');
 var express = require('express');
 var app = express();
 const axios = require("axios");
+const cheerio = require("cheerio");
 var convert = require('xml-js');
 fs = require('fs');
 const ics = require('ics')
@@ -114,9 +115,7 @@ async function scrapeData() {
     console.error(err);
   }
 }
-cron.schedule('0 0 */1 * * *', () => {
-    console.log('running a task evry hour')
-})
+setInterval(scrapeData, 60*1000);
 // Invoke the above function
 scrapeData();
 
